e0439299e1f1696b53e8af8bc4dc86480e461611
Zac Hunsaker <zkh3@georgetown.edu>
2026-01-14 16:44:07 -0600

    update checksums for v0.7.0

--- checksum-Elixir.DomainTwistex.Utils.exs
+++ checksum-Elixir.DomainTwistex.Utils.exs
@@ -1,8 +1,8 @@
 %{
-  "domaintwistex-v0.6.1-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:cf84c6529a75631a004b4e74312b1087700395c1e9f4826b609daa85e8be3af8",
-  "domaintwistex-v0.6.1-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:d425cc1745a96769515119629f5c9512375f2c5e4f37a53535374321d0f8aef8",
-  "libdomaintwistex-v0.6.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:d2859584ac53756a9d8f16a817621ad62403b54aae9cc62f7245ffad7bf239c8",
-  "libdomaintwistex-v0.6.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:ec7ea537caf6fae860eea8965320084e71ffd7761fb520dacfd4a5b9fef6b70d",
-  "libdomaintwistex-v0.6.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:be3e4193012e6d817954e500bf784d9f1a18aee3cdafc1e1c62090b653d6afa6",
-  "libdomaintwistex-v0.6.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:4f55b9a23ebd24052a7c3309be3c57d81a83895cff926ffb963f23ecb29d7be1",
+  "domaintwistex-v0.7.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:cb666842c5a12ca300f7fbd72bf8317a4f027cc09ebdb6a978747dc6920693dd",
+  "domaintwistex-v0.7.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:b556d7fe6edeef2c76ec15be94bc0aa525330f834acf4585f94ec17758140aa2",
+  "libdomaintwistex-v0.7.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:93de35f865a988ee8d69aa49b7ec126963c220cda92b9ddda83df6962f94ee28",
+  "libdomaintwistex-v0.7.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:6ae9e6f34446bbfa15863fd3ef76362e5d205385ec844f8eca25b13ca07e1009",
+  "libdomaintwistex-v0.7.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:fad8d84f7e60ce5427570db1e87ad7b3d790410eb538863744265caa47437dbe",
+  "libdomaintwistex-v0.7.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:93b105a790f7dba88431b95a324302f5b0b660f6b6d17cd05daa15d88016f72b",
 }
